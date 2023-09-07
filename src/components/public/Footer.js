f47856640b8dc70359de21ffb52762180302084e
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2023-09-07 18:46:01 -0500

    react-video2-1-7-sep-2023

--- src/components/public/Footer.js
+++ src/components/public/Footer.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Footer = () => {
+
+  const [clicks, setClicks]=useState(0);
+
+  const year = new Date().getFullYear();
+  const companyName = "EGG Education";
+
+  const hanleClick = () => {
+    setClicks(clicks + 1);
+  }
+
   return (
     <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p className="col-md-4 mb-0 text-body-secondary">&copy; 2023 Company, Inc</p>
+        <p className="col-md-4 mb-0 text-body-secondary">&copy; {year} {companyName} Clicks={clicks}</p>
+
+        <spam  
+          className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark"
+          onClick={hanleClick}
+        >
+          <img
+              className='App-logo'
+              height="52"
+              src="dog_6138615.png"
+              alt='perro-giratorio'/>
+        </spam>
+
         <ul className="nav col-md-4 justify-content-end">
           <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
           <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
@@ -14,6 +36,5 @@ const Footer = () => {
     </footer>
   )
 }
-
 export default Footer
 
--- src/components/public/Navbar.js
+++ src/components/public/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
                     <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     
                     <span className='d-flex align-items-center mb-2 mb-lg-0 text-decoration-none'>
-                    <img className='App-logo' height='52' src="dog_6138615.png"/>
+                    <img className='App-logo' height='52' src='dog_6138615.png'/>
                     </span>
                     <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                         <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
